a89194afbe203c58059a8d85a9c04e1ad938b308
getuliobr <getulioregis@alunos.utfpr.edu.br>
2021-11-16 02:23:35 -0300

    mensagem quando o servidor abrir

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Servidor rodando na porta: ${process.env.APP_PORT}`);
+});
 
 require('./src/routes/index')(app)
 
