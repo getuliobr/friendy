18d750a70714eef86d5f687c35bd43041784f62c
Arthur <arthurhan269@gmail.com>
2021-11-21 20:04:20 -0300

    Adicionar userContext e refatorar código para uso do context.

---
+++ Frontend/src/contexts/userContext.js
@@ -0,0 +1,75 @@
+import {createContext, useEffect, useState} from "react";
+import Cookies from 'universal-cookie';
+import { toast } from 'react-toastify'
+import axios from "axios";
+export const UserContext = createContext({});
+
+export const UserProvider = ({children}) => {
+    const cookie = new Cookies();
+    const [userName, setUserName] = useState(cookie.get("userName"));
+    const [userToken, setUserToken] = useState(cookie.get("token"));
+    const [userId, setUserId] = useState(cookie.get("userId"));
+
+    const api = axios.create({
+        baseURL: "http://localhost:3333",
+        headers: { Authorization: userToken }
+    })
+
+    const handleLogin = async (username, password) => {
+        try {
+            const { data: { token, usuario: { id, nome } } } = await api.post("/usuario/login", {
+                nome: username,
+                senha: password
+            })
+            cookie.set("token", token);
+            cookie.set("userId", id);
+            cookie.set("userName", nome);
+            setUserToken(token);
+            setUserName(nome);
+        } catch (e) {
+            toast.error("Erro ao logar.")
+        }
+    }
+
+    const handleRegister = async (username, password) => {
+        try {
+            const { data: { token, usuario: { id, nome } } } = await api.post("/usuario/cadastrar", {
+                nome: username,
+                senha: password
+            })
+            cookie.set("token", token);
+            cookie.set("userId", id);
+            cookie.set("userName", nome);
+            setUserToken(token);
+            setUserName(nome);
+        } catch (e) {
+            toast.error("Erro ao cadastrar.")
+        }
+    }
+
+    const getFollowing = async () => {
+        try {
+            await api.get("/seguindo", {
+                id: userId,
+            })
+        } catch (e) {
+            toast.error("Ocorreu um erro ao buscar quem você segue.")
+        }
+    }
+
+    const logout = () => {
+        cookie.remove("token");
+        cookie.set("userId");
+        cookie.set("userName");
+        setUserToken(undefined);
+        setUserName(undefined);
+    }
+
+    return (
+        <UserContext.Provider
+            value={{userName, userToken, handleLogin, handleRegister, logout, getFollowing}}
+        >
+            {children}
+        </UserContext.Provider>
+    )
+}
--- Frontend/src/screens/LoginPage/index.js
+++ Frontend/src/screens/LoginPage/index.js
@@ -1,68 +1,7 @@
-import axios from "axios";
-import React, { useState } from "react";
 import { LoginComponent } from "../../components/loginComponent";
-import Cookies from "universal-cookie";
-import { toast } from 'react-toastify'
-import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirmed, setPasswordConfirmed] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const cookie = new Cookies();
-    const navigate = useNavigate();
-
-    const handleLogin = async () => {
-        try {
-            setLoading(true);
-            const { data: { token, usuario: { id, nome } } } = await axios.post("http://localhost:3333/usuario/login", {
-                nome: username,
-                senha: password
-            })
-            cookie.set("token", token);
-            cookie.set("userId", id);
-            cookie.set("userName", nome);
-
-            navigate('/home')
-        } catch (e) {
-            toast.error("Erro ao logar.")
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    const handleRegister = async () => {
-        try {
-            setLoading(true);
-            const { data: { token, usuario: { id, nome } } } = await axios.post("http://localhost:3333/usuario/cadastrar", {
-                nome: username,
-                senha: password
-            })
-            cookie.set("token", token);
-            cookie.set("usuarioId", id);
-            cookie.set("usuarioNome", nome);
-
-            navigate('/home')
-        } catch (e) {
-            toast.error("Erro ao cadastrar.")
-        } finally {
-            setLoading(false);
-        }
-    }
-
     return (
-        <LoginComponent
-            username={username}
-            setUsername={setUsername}
-            password={password}
-            setPassword={setPassword}
-            passwordConfirmed={passwordConfirmed}
-            setPasswordConfirmed={setPasswordConfirmed}
-            handleLogin={handleLogin}
-            handleRegister={handleRegister}
-            loading={loading}
-        />
+        <LoginComponent />
     )
 }
